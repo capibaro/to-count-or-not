ceff37b18aa7eebf5ae8efe04ed3b4552a1ca2c2
maotouyingxia <xiangxinxx@outlook.com>
2021-03-28 14:23:25 +0800

    spring initialize

---
+++ src/main/java/com/comp2024b/tocountornot/ToCountOrNotApplication.java
@@ -0,0 +1,13 @@
+package com.comp2024b.tocountornot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToCountOrNotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToCountOrNotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comp2024b/tocountornot/ToCountOrNotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comp2024b.tocountornot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToCountOrNotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
